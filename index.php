c0495dbf8a99da0aef0aef6ab2cd6c6fd3f3b469
JPCanoy <36150057+JPCanoy@users.noreply.github.com>
2018-02-06 10:32:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>workshop</title>
+</head>
+<body style="background-color:powderblue;">>
+
+<h1><center>Biography</center></h1>
+<p>My name is John Philip C. LAngamin and I am currently studying at Cebu Technological University, Naga Extension Campus taking up Bachelor of Science  Information and Communication Technology. I am majoring in Programming.. I am the first person to
+go to college and have really loved my experience at CTU-NAga! I come from a large family and am
+the oldest of three children. Although I am far from
+home, I feel I made a good choice attending CTU-Naga CAmpus because I've learned how to be independent and have
+more confidence in learning new things on my own.</p>
+
+</body>
+</html>
